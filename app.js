035372a7c98fd15152d10045a6f9ca2bcfa140f1
Sunatullo Gafurov <me@gmail.com>
2020-02-21 17:45:36 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+let nextPostId = 4;
+let posts = [
+    {id: 1, name: 'Me'},
+    {id: 2, name: 'You'},
+    {id: 3, name: 'Us'}
+];
+
+app.get('/api/posts', (req, res) => {  
+    res.send(posts);
+    console.log(posts);
+});
+
+
+
+app.post('/api/posts', (req, res) => {
+    const body = req.body;
+    if (body.id === 0) {
+        posts = [...posts, {...body, id: nextPostId++}];
+        res.send(posts);
+        return;
+    } else {
+        posts = posts.map(i => i.id === body.id ? {...i, name: body.name} : i);
+        return;
+    }
+});
+
+app.delete('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    posts = posts.filter(i => i.id !== id);
+    res.send(posts);
+    console.log(posts);
+});
+
+
+app.listen(process.env.port || 4000);
